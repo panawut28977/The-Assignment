5c10d5dd617aa97992aa9ee45561c606fae40d23
Shinomiya <Shinomiya@192.168.2.106>
2014-08-12 20:14:30 +0700

    StAssignmentFile -> All Class through Class diagram + Excel file reader
    jub jub

--- src/java/Model/StAssignmentOnWeb.java
+++ src/java/Model/StAssignmentOnWeb.java
@@ -6,8 +6,15 @@
 
 package Model;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -87,4 +94,93 @@ public class StAssignmentOnWeb {
         this.anwerQuestion = anwerQuestion;
     }
     
+    public double getScore(int st_am_id){
+        Connection conn = ConnectionBuilder.getConnection();
+        String sql = "select score from student_assignment_on_web where st_ass_id = ? ";
+        PreparedStatement pstm;
+        double result = 0;
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, st_am_id);
+            ResultSet rs = pstm.executeQuery();
+            if (rs.next()) {
+                result = rs.getDouble(1);
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StAssignmentOnWeb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    public List<StAssignmentOnWeb> getStAmInfo(int st_am_id){
+        List<StAssignmentOnWeb> StAssInfo = new ArrayList<StAssignmentOnWeb>();
+        Connection conn = ConnectionBuilder.getConnection();
+        String sql = "select * from student_assignment_on_web where st_ass_id = ? order by send_date desc fetch first 1 rows";
+        PreparedStatement pstm;
+        StAssignmentOnWeb saw = null;
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, st_am_id);
+            ResultSet rs = pstm.executeQuery();
+            while (rs.next()) {
+                saw = new StAssignmentOnWeb();
+                saw.setAcc_id(rs.getInt("acc_id"));
+                saw.setAm_id(rs.getInt("ass_id"));
+                saw.setSt_am_id(st_am_id);
+                saw.setSend_date(rs.getDate("send_date"));
+                saw.setScore(rs.getDouble("score"));
+                StAssInfo.add(saw);
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StAssignmentOnWeb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return StAssInfo;
+    }
+    
+    public double getScoreByAccIDAndAmID(int acc_id , int am_id){
+        Connection conn = ConnectionBuilder.getConnection();
+        String sql = "select score from student_assignment_on_web where ass_id = ? and acc_id = ?";
+        PreparedStatement pstm;
+        double result = 0;
+        
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, acc_id);
+            pstm.setInt(2, am_id);
+            ResultSet rs = pstm.executeQuery();
+            if (rs.next()) {
+                result = rs.getDouble(1);
+            }
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StAssignmentOnWeb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return result;
+    }
+    
+    public void sendAm(List<StAssignmentOnWeb> StAssWeb){
+        Connection conn = ConnectionBuilder.getConnection();
+        String sql = "insert into Student_assignment_on_web(st_ass_id,ass_id,acc_id,score,send_date) values(?,?,?,?,?)";
+        PreparedStatement pstm;
+        int result = 0;
+        try {
+            pstm = conn.prepareStatement(sql);
+            pstm.setInt(1, StAssWeb.get(0).getSt_am_id());
+            pstm.setInt(2, StAssWeb.get(1).getAm_id());
+            pstm.setInt(3, StAssWeb.get(2).getAcc_id());
+            pstm.setDouble(4, StAssWeb.get(4).getScore());
+            pstm.setDate(5, (java.sql.Date) StAssWeb.get(5).getSend_date());
+            result = pstm.executeUpdate();
+            conn.close();
+        } catch (SQLException ex) {
+            Logger.getLogger(StAssignmentOnWeb.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+    
+    public void autoChecking(int st_am_id){
+        
+    }
 }
